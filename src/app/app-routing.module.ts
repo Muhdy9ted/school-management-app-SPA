ef6cba3c58cf3438f1cafa180f08ee84d162f9b4
muhammedu9ted@outlook.com <muhammedu9ted@outlook.com>
2020-07-19 10:37:10 +0100

    completed the register, login and jwt modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { AccountComponent } from './components/account/account.component';
+import { RegisterComponent } from './components/account/register/register.component';
+import { LoginComponent } from './components/account/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LandingPageComponent},
+  {path: 'account', component: AccountComponent, children: [
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'register', component: RegisterComponent},
+    {path: 'login', component: LoginComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './_shared/services/auth.service';
+import { Router, ActivatedRoute, NavigationStart, NavigationEnd, NavigationError, Event, NavigationCancel } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SchoolManagementApp-SPA';
+  showSpinner = false;
+
+
+  constructor(private authService: AuthService, private router: Router, private route: ActivatedRoute) {
+    this.router.events.subscribe((routerEvent: Event) => {
+      if (routerEvent instanceof NavigationStart) {
+        this.showSpinner = true;
+      } else if (routerEvent instanceof NavigationEnd
+        || routerEvent instanceof NavigationError
+        || routerEvent instanceof NavigationCancel) {
+        this.showSpinner = false;
+      }
+    });
+  }
 }
